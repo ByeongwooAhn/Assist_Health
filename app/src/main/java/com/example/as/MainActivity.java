c9d051e0f40e414e16d02dcbdd369914b6a46eae
DESKTOP-DLKDAER\am608 <abyungwoo@gmail.com>
2024-06-10 03:47:15 +0900

    상단바 디자인 변경

--- app/src/main/java/com/example/as/MainActivity.java
+++ app/src/main/java/com/example/as/MainActivity.java
@@ -45,5 +45,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button sikdanListButton = findViewById(R.id.sikdan_list);
+        sikdanListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, sikdan_list.class);
+                startActivity(intent);
+            }
+        });
     }
 }
