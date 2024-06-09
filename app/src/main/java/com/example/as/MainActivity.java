7cc322530afc45d1a5d3b8427254c07fadee2b43
DESKTOP-DLKDAER\am608 <abyungwoo@gmail.com>
2024-06-09 23:54:17 +0900

    식단 구성 - 단백질 추가 식단 캘린더 추가

--- app/src/main/java/com/example/as/MainActivity.java
+++ app/src/main/java/com/example/as/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.as;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.sikdan_main);
+
+        // Button 클릭 리스너 설정
+        Button sikdanMakeButton = findViewById(R.id.sikdan_make);
+        sikdanMakeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sikdan), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button sikdanCalendarButton = findViewById(R.id.sikdan_calender);
+        sikdanCalendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, sikdan_calender.class);
+                startActivity(intent);
+            }
         });
     }
 }
